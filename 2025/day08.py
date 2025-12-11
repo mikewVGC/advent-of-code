6cc9afedb9033d211cb0bef8cd175f337a8a5ea8
Mike W <mike.watson+vgc@gmail.com>
2025-12-10 23:33:12 -0500

    Day 8 part 1... but it's super slow

--- 2025/day08.py
+++ 2025/day08.py
@@ -1,13 +1,13 @@
 
 import math
 
-
 class Connection():
     def __init__(self, node1, node2):
         self.node1 = node1
         self.node2 = node2
         self.distance = self.calc_distance(node1, node2)
         self.enabled = False
+        self.dupe_check = False
 
     def calc_distance(self, node1, node2):
         return math.sqrt(
@@ -38,7 +38,7 @@ class Node():
 
 
 coords = {}
-with open("2025/input/day08-example", "r") as file:
+with open("2025/input/day08", "r") as file:
     lines = file.read().splitlines()
     for line in lines:
         coord = list(map(lambda x: int(x), line.split(',')))
@@ -66,58 +66,99 @@ shortlist = []
 while True:
     for coord in coords.values():
         for code, connection in coord.connections.items():
-            if shortest == None or (connection.distance < shortest.distance and connection.enabled == False):
+            if shortest == None or (connection.distance < shortest.distance and connection.enabled == False and connection.dupe_check == False):
                 shortest = connection
 
-    shortest.enabled = True
-    shortlist.append(shortest)
+    dupe_found = False
+    for short in shortlist:
+        if (
+            short.node1.get_code() == shortest.node2.get_code() and
+            short.node2.get_code() == shortest.node1.get_code()
+        ):
+            dupe_found = True
+            break
+
+    if dupe_found == False:
+        shortest.enabled = True
+        shortlist.append(shortest)
+    else:
+        shortest.dupe_check = True
+
     shortest = None
 
-    if len(shortlist) == 10:
+    if len(shortlist) == 1000: # change this to 10 for example
         break
 
-for conn in shortlist:
-    print(conn.node1.get_code(), conn.node2.get_code())
-print('====')
-
+    # this loop is slow so adding a counter... easier than fixing it
+    if len(shortlist) % 100 == 0:
+        print(f"{len(shortlist)} shorties so far")
 
 # now figure out how many clusters we have
 clusters = []
 for conn in shortlist:
     if len(clusters) == 0:
-        clusters.append([ conn ])
+        clusters.append({
+            conn.node1.get_code(): True,
+            conn.node2.get_code(): True,
+        })
         continue
 
     # very dumb, but search clusters
+    found_cluster = False
     for cluster in clusters:
         add_to_current_cluster = False
-        new_cluster = False
-        for c in cluster:
+        for c in cluster.keys():
             if (
-                c.node1.get_code() == conn.node1.get_code() or
-                c.node1.get_code() == conn.node2.get_code() or
-                c.node2.get_code() == conn.node1.get_code() or
-                c.node2.get_code() == conn.node2.get_code()
+                c == conn.node1.get_code() or
+                c == conn.node2.get_code()
             ):
                 add_to_current_cluster = True
                 break
-            else:
-                new_cluster = True
-                break
 
         if add_to_current_cluster:
-            cluster.append(conn)
-            break
+            found_cluster = True
+            cluster[conn.node1.get_code()] = True
+            cluster[conn.node2.get_code()] = True
 
-        if new_cluster:
-            clusters.append([ conn ])
-            break
+    if found_cluster == False:
+        clusters.append({
+            conn.node1.get_code(): True,
+            conn.node2.get_code(): True,
+        })
 
-result = 0
+# merge clusters that share nodes
+while True:
+    ops = []
+    for i, cluster in enumerate(clusters):
+        for c in cluster.keys():
+            for ii, ccluster in enumerate(clusters):
+                if ii == i:
+                    continue
+
+                for cc in ccluster.keys():
+                    if cc == c:
+                        ops.append([ i, ii ])
+
+    if len(ops) == 0:
+        break
+
+    for op in ops:
+        clusters[op[0]].update(clusters[op[1]])
+        clusters[op[1]] = {}
+
+    ops = []
+
+
+# okay last loop, lets count how many unique nodes are in each cluster
+cluster_counts = []
 for cluster in clusters:
-    #print(len(cluster))
-    for c in cluster:
-        print(c.node1.get_code(), c.node2.get_code())
-    print('---')
+    cluster_counts.append(len(cluster.keys()))
+
+cluster_counts.sort(reverse=True)
+
+# jk one more to get the top 3
+result = 1
+for i in cluster_counts[:3]:
+    result *= i
 
 print(f"Part 1: {result}")
